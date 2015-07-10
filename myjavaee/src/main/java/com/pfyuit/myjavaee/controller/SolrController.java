24426b9b61e6aa3bfdf5311be8f7df58a5c1ca58
yupengfei <yupengfei@lightinthebox.com>
2015-07-10 16:48:35 +0800

    added solr use case

---
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/controller/SolrController.java
@@ -0,0 +1,26 @@
+package com.pfyuit.myjavaee.controller;
+
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pfyuit.myjavaee.service.SolrService;
+
+@Controller
+@RequestMapping(value = { "/solr" })
+public class SolrController {
+
+	@Autowired
+	private SolrService solrService;
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Integer> searchBlog() throws SolrServerException {
+		return solrService.searchBlog("mysql");
+	}
+}
---
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/service/SolrServiceImpl.java
@@ -0,0 +1,49 @@
+package com.pfyuit.myjavaee.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pfyuit.myjavaee.model.SearchItem;
+
+@Service
+public class SolrServiceImpl implements SolrService {
+
+	@Autowired
+	private SolrConfig solrConfig;
+
+	private static final String DEFAULT_QUERY_FIELD = "blogcontent";
+	private static final String DEFAULT_RETURN_FIELD = "id";
+
+	@Override
+	public List<Integer> searchBlog(String key) throws SolrServerException {
+		HttpSolrServer server = new HttpSolrServer(solrConfig.getUrl());
+		server.setSoTimeout(Integer.parseInt(solrConfig.getSoTimeout()));
+		server.setConnectionTimeout(Integer.parseInt(solrConfig.getConnectionTimeout()));
+		server.setDefaultMaxConnectionsPerHost(Integer.parseInt(solrConfig.getDefaultMaxConnectionsPerHost()));
+		server.setMaxTotalConnections(Integer.parseInt(solrConfig.getMaxTotalConnections()));
+		server.setFollowRedirects(false);
+		server.setAllowCompression(true);
+		server.setMaxRetries(1);
+
+		SolrQuery query = new SolrQuery();
+		query.setQuery(DEFAULT_QUERY_FIELD + ":" + key);
+		query.addField(DEFAULT_RETURN_FIELD);
+		query.addSort(DEFAULT_RETURN_FIELD, SolrQuery.ORDER.asc);
+
+		QueryResponse response = server.query(query);
+		List<SearchItem> items = response.getBeans(SearchItem.class);
+
+		List<Integer> result = new ArrayList<Integer>();
+		for (SearchItem item : items) {
+			result.add(Integer.valueOf(item.getId()));
+		}
+		return result;
+	}
+}
