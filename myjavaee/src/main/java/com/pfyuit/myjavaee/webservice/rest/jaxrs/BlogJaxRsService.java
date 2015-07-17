571e9a0267154d51ff5707358b289e977f35c74d
yupengfei <yupengfei@lightinthebox.com>
2015-07-17 19:23:11 +0800

    add rest jaxrs use case

---
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/webservice/rest/jaxrs/BlogJaxRsService.java
@@ -0,0 +1,28 @@
+package com.pfyuit.myjavaee.webservice.rest.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pfyuit.myjavaee.dto.business.BlogDto;
+import com.pfyuit.myjavaee.dto.business.BlogDtoBuilder;
+import com.pfyuit.myjavaee.model.jpa.BlogModel;
+import com.pfyuit.myjavaee.service.business.BlogService;
+
+@Path("/blogservice")
+public class BlogJaxRsService {
+
+	@Autowired
+	private BlogService blogService;
+
+	@GET
+	@Path("/blogs")
+	@Produces("application/json; charset=UTF-8")
+	public BlogDto findBlogById(@QueryParam("id") Integer id) {
+		BlogModel model = blogService.findById(id);
+		return BlogDtoBuilder.buildBlogDto(model);
+	}
+}
