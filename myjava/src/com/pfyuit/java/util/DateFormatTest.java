139bf657ba297a3433d5aad52ab2f6f93189f202
yupengfei <yupengfei@lightinthebox.com>
2015-09-11 15:57:42 +0800

    add Timestamp and Dateformat cases

---
+++ myjava/src/com/pfyuit/java/util/DateFormatTest.java
@@ -0,0 +1,61 @@
+package com.pfyuit.java.util;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class DateFormatTest {
+
+	public static void main(String[] args) throws ParseException {
+		testFormat();
+		testParse();
+	}
+
+	private static void testFormat() {
+		System.out.println("==>testFormat");
+		Timestamp ts = Timestamp.valueOf("2015-09-11 08:00:00");
+		System.out.println("original timestamp: " + ts.getTime());
+		System.out.println();
+
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT-8"));
+		String formattedTime = formatter.format(ts);
+		System.out.println("formatted string with GMT-8 timezone: " + formattedTime);
+		System.out.println();
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+		formattedTime = formatter.format(ts);
+		System.out.println("formatted string with GMT timezone: " + formattedTime);
+		System.out.println();
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+		formattedTime = formatter.format(ts);
+		System.out.println("formatted string with GMT+8 timezone: " + formattedTime);
+		System.out.println();
+	}
+
+	private static void testParse() throws ParseException {
+		System.out.println("==>testParse");
+		String timeStr = "2015-09-11 08:00:00";
+
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT-8"));
+		Date date = formatter.parse(timeStr);
+		System.out.println(date);// JVM默认时区
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+		date = formatter.parse(timeStr);
+		System.out.println(date);// JVM默认时区
+
+		formatter.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+		date = formatter.parse(timeStr);
+		System.out.println(date);// JVM默认时区
+	}
+
+}
