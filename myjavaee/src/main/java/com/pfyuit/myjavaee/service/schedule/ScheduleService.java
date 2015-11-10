228dcb997444b99f3c94e2f1885dcba124490d8e
yupengfei <yupengfei@lightinthebox.com>
2015-11-10 11:46:05 +0800

    add schedule module

---
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/service/schedule/ScheduleService.java
@@ -0,0 +1,12 @@
+package com.pfyuit.myjavaee.service.schedule;
+
+/**
+ * Schedule service interface to provide business logic to the schedule
+ * framework (typically Quartz).
+ * @author yupengfei
+ */
+public interface ScheduleService {
+
+	public abstract void execute();
+
+}
