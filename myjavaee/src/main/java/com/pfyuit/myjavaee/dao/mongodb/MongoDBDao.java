e1fe8cbf5c7f0bb0211ca85a61fc89e9bd6c89c8
yupengfei <yupengfei@lightinthebox.com>
2015-11-10 18:35:00 +0800

    add mongodb module

---
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/dao/mongodb/MongoDBDao.java
@@ -0,0 +1,41 @@
+package com.pfyuit.myjavaee.dao.mongodb;
+
+import java.util.List;
+import java.util.Set;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
+/**
+ * MongoDB Dao interface. Including collections operations.
+ * @author yupengfei
+ */
+public interface MongoDBDao {
+
+	public abstract DB getDB();
+
+	public abstract DBCollection createCollection(String collectionName);
+
+	public abstract <T> DBCollection createCollection(Class<T> type);
+
+	public abstract void dropCollection(String collectionName);
+
+	public abstract <T> void dropCollection(Class<T> type);
+
+	public abstract boolean isCollectionExists(String collectionName);
+
+	public abstract <T> boolean isCollectionExists(Class<T> type);
+
+	public abstract DBCollection getCollection(String collectionName);
+
+	public abstract Set<String> getCollections();
+
+	public abstract <T> void save(T model);
+
+	public abstract <T> void remove(T model);
+
+	public abstract <T> List<T> findAll(Class<T> type);
+
+	public abstract <T> T findById(int id, Class<T> type);
+
+}
