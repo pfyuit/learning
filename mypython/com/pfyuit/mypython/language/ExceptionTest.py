d7be0170857e2a2a1131c2102ff827ff109c3332
yupengfei <yupengfei@lightinthebox.com>
2016-02-01 17:28:13 +0800

    python update

---
+++ mypython/com/pfyuit/mypython/language/ExceptionTest.py
@@ -0,0 +1,24 @@
+'''
+Created on Feb 1, 2016
+@author: yupengfei
+'''
+
+try:
+   fh = open("C:\\abc.txt", "w")
+   1/0
+   fh.write("This is my test file for exception handling!!")
+except IOError:
+   print("Error: can\'t find file or read data")
+except BaseException:
+   print("Error: unexpected exception")
+else:
+   print("Written content in the file successfully")
+
+
+try:
+   fh = open("C:\\abc.txt", "w")
+   fh.write("This is my test file for exception handling!!")
+except BaseException:
+   print("Error: unexpected exception")
+finally:
+   print("Error: can\'t find file or read data")
--- mypython/com/pfyuit/mypython/library/FileTest.py
+++ mypython/com/pfyuit/mypython/library/FileTest.py
@@ -3,9 +3,22 @@ Created on Jan 29, 2016
 @author: yupengfei
 '''
 
+import os
+
 file=open("C:\\abc.txt", "rb")
 print("file name:",file.name)
 print("file isclosed:",file.closed)
 print("file access mode:",file.mode)
+
+str =file.read(10)
+print("read:",str)
+
+position = file.tell();
+print(position)
+
+os.mkdir("C:\\test")
+os.rmdir("C:\\test")
+
+#os.rename( "C:\\abc.txt", "C:\\test.txt" )
 
 file.close()
