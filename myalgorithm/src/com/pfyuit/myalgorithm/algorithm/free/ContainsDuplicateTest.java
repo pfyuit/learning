1d774a29e4d9f1b2125bb53faea0fb820a4d20f9
yupengfei <yupengfei@lightinthebox.com>
2016-02-04 18:53:02 +0800

    new algorithms

---
+++ myalgorithm/src/com/pfyuit/myalgorithm/algorithm/free/ContainsDuplicateTest.java
@@ -0,0 +1,33 @@
+package com.pfyuit.myalgorithm.algorithm.free;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of integers, find if the array contains any duplicates. Your function should return true if any value
+ * appears at least twice in the array, and it should return false if every element is distinct.
+ * 核心思想：先快排，再找重复数字
+ * @author yupengfei
+ */
+public class ContainsDuplicateTest {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] { 2, 4, 5, 7, 8, 2, 5 };
+		System.out.println(containsDuplicate(nums));
+	}
+
+	public static boolean containsDuplicate(int[] nums) {
+		if(nums.length==0){
+			return false;
+		}
+		Arrays.sort(nums);
+		int previous = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (previous == nums[i]) {
+				return true;
+			}
+			previous = nums[i];
+		}
+		return false;
+	}
+
+}
