f20df8bb619d07449b4b5ac3de633acff8f16da5
yupengfei <yupengfei@lightinthebox.com>
2016-06-05 18:05:37 +0800

    change logback for framework

--- myjavaee/src/main/java/com/pfyuit/myjavaee/web/mvc/springmvc/WebController.java
+++ myjavaee/src/main/java/com/pfyuit/myjavaee/web/mvc/springmvc/WebController.java
@@ -2,10 +2,14 @@ package com.pfyuit.myjavaee.web.mvc.springmvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.pfyuit.myjavaee.model.database.rdbms.hibernate.BlogModel;
+import com.pfyuit.myjavaee.service.database.rdbms.hibernate.BlogService;
 
 /**
  * Spring MVC controller. It typically used in the Java web design. Access
@@ -18,9 +22,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class WebController {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebController.class);
+	
+	@Autowired
+	private BlogService blogService;
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView testGet() {
+		BlogModel model = blogService.findById(3);
+
 		logger.info("index");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("index");
