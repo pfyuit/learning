2241713fad203b6c3d05c369b545b585e0b5124e
yupengfei <yupengfei@lightinthebox.com>
2016-06-10 23:15:25 +0800

    some refactors

--- myjavaee/src/main/resources/database/rdbms/mysql/pinche.sql
+++ myjavaee/src/main/resources/database/rdbms/mysql/pinche.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: icarving_pinche
+-- Host: localhost    Database: pinche
 -- ------------------------------------------------------
 -- Server version	5.6.17
 
